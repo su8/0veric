852a635cc9c6729a74653df47214dab546dcba7c
su8 <wifiextender2@gmail.com>
2025-12-22 13:11:26 +0300

    reduce empty lines

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@
 #include <readline/history.h>
 
 // ANSI color codes
-#define COLOR_RESET   "\033[0m"
+#define COLOR_RESET   "\033[0;0m"
 #define COLOR_GREEN   "\033[32m"
 #define COLOR_YELLOW  "\033[33m"
 #define COLOR_CYAN    "\033[36m"
@@ -60,23 +60,18 @@ int main(int argc, char *argv[]) {
     std::cerr << "Usage: " << argv[0] << " <server> <port> <nick> <channel1> <channel2> ...\n";
     return EXIT_FAILURE;
   }
-
   std::string server = argv[1];
   int port = std::stoi(argv[2]);
   std::string nick = argv[3];
   std::string user = nick + " 0 * :" + nick;
-
   for (int x = 4; x < argc; x++) {
     channels.push_back(argv[x]);
   }
-
   // Setup Readline tab-completion
   rl_attempted_completion_function = channelCompletion;
-
   // Connect to IRC
   int sockfd = tcpConnect(server, port);
-  if (sockfd < 0) return EXIT_FAILURE;
-
+  if (sockfd < 0) { return EXIT_FAILURE; }
   SSL_library_init();
   SSL_load_error_strings();
   OpenSSL_add_all_algorithms();
@@ -87,10 +82,8 @@ int main(int argc, char *argv[]) {
     close(sockfd);
     return EXIT_FAILURE;
   }
-
   SSL *ssl = SSL_new(ctx);
   SSL_set_fd(ssl, sockfd);
-
   if (SSL_connect(ssl) <= 0) {
     ERR_print_errors_fp(stderr);
     SSL_free(ssl);
@@ -98,9 +91,7 @@ int main(int argc, char *argv[]) {
     close(sockfd);
     return EXIT_FAILURE;
   }
-
   std::cout << COLOR_GREEN << "Connected securely to " << server << " via TLS.\n" << COLOR_RESET;
-
   // IRC handshake
   sendIRC(ssl, "NICK " + nick);
   sendIRC(ssl, "USER " + user);
@@ -115,10 +106,8 @@ int main(int argc, char *argv[]) {
     fd_set readfds;
     FD_ZERO(&readfds);
     FD_SET(sockfd, &readfds);
-
     struct timeval tv{0, 100000}; // 100ms
     int activity = select(sockfd + 1, &readfds, nullptr, nullptr, &tv);
-
     // Handle server messages
     if (activity > 0 && FD_ISSET(sockfd, &readfds)) {
       std::memset(buffer, 0, sizeof(buffer));
@@ -129,24 +118,19 @@ int main(int argc, char *argv[]) {
       }
       std::string msg(buffer);
       parseAndPrintMessage(msg);
-
       // Respond to PING
       if (msg.find("PING") == 0) {
         sendIRC(ssl, "PONG " + msg.substr(5));
       }
     }
-
     // Handle user input (blocking readline)
     char *line = readline("> ");
     if (!line) break; // EOF (Ctrl+D)
-
     std::string input(line);
     free(line);
-
     if (!input.empty()) {
       add_history(input.c_str());
     }
-
     if (input == "/quit") {
       sendIRC(ssl, "QUIT :Bye!");
       break;
@@ -178,13 +162,11 @@ int main(int argc, char *argv[]) {
       }
     }
   }
-
   // Cleanup
   SSL_shutdown(ssl);
   SSL_free(ssl);
   SSL_CTX_free(ctx);
   close(sockfd);
-
   return EXIT_SUCCESS;
 }
 
@@ -193,22 +175,18 @@ int tcpConnect(const std::string &server, int port) {
   int sockfd;
   struct sockaddr_in serv_addr{};
   struct hostent *server_host;
-
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     perror("Socket creation failed");
     return -1;
   }
-
   if ((server_host = gethostbyname(server.c_str())) == nullptr) {
     std::cerr << "No such host: " << server << std::endl;
     close(sockfd);
     return -1;
   }
-
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(port);
   std::memcpy(&serv_addr.sin_addr.s_addr, server_host->h_addr, server_host->h_length);
-
   if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
     perror("Connection failed");
     close(sockfd);
@@ -236,12 +214,10 @@ void setNonBlocking(int fd) {
 char *channelNameGenerator(const char *text, int state) {
   static size_t list_index;
   static size_t len;
-
   if (!state) {
     list_index = 0;
     len = strlen(text);
   }
-
   while (list_index < channels.size()) {
     const std::string &name = channels[list_index++];
     if (name.compare(0, len, text) == 0) {
@@ -254,7 +230,6 @@ char *channelNameGenerator(const char *text, int state) {
 // Completion function for Readline
 char **channelCompletion(const char *text, int start, int end) {
   rl_attempted_completion_over = 1;
-
   // Only complete after /msg or /part
   if (start >= 5 && (strncmp(rl_line_buffer, "/msg ", 5) == 0 || strncmp(rl_line_buffer, "/part ", 6) == 0)) {
     return rl_completion_matches(text, channelNameGenerator);
